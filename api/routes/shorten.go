65203a37d87406a539858cba2b43994fd9d5b9e5
Shaunak Sensarma <sensarmashaunak@gmail.com>
2024-02-11 12:41:49 +0530

    More methods related to routes and helpers. (#9)
    * Creating main function
    * Creating db routes

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -15,3 +15,26 @@ type response struct {
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"eror": "cannot parse JSON"})
+	}
+
+	//implement rate limiting.
+
+	//check if the input is an actual URL.
+	if !goValidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	//check for domain error.
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "You have gone to bad address"})
+	}
+
+	//enforce https, SSL.
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
